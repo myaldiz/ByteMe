264d2b77e403076d989c831ee7feeac6038c3144
Anders <anders.wk.choi@gmail.com>
2018-10-30 23:05:36 +0900

    reuploaded correct server folder

--- server/byteme/byteme/settings.py
+++ server/byteme/byteme/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b741%-b6v@edbp4@9z1y94jhok3t$!526zn2^u$ta51%xj7-*-'
+SECRET_KEY = '*1&_#r3=9%lj%8f9)#k*lkz8l+n=-h#2)@fgw(m%1c=7qu0f+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
