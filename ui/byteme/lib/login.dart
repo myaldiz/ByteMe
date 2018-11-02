42ae4d95cf900483a07282804e0fb8c0a17d5328
Mohamed Elgaar <mohamed@kaist.ac.kr>
2018-11-02 20:38:40 +0900

    progess on login/signup

--- ui/byteme/lib/login.dart
+++ ui/byteme/lib/login.dart
@@ -8,6 +8,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginPage> {
+  static final emailController = TextEditingController();
+  static final passwordController = TextEditingController();
+  String email, password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,44 +23,62 @@ class _LoginState extends State<LoginPage> {
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              FlutterLogo(size: 50.0),
-              SizedBox(height: 50.0),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-              ),
-              SizedBox(height: 35.0),
-              RaisedButton(
-                child: Text("Log in"),
-                onPressed: () {
-                  Navigator.of(context).popAndPushNamed('/');
-                },
-              ),
-              SizedBox(height: 7.0),
-              RaisedButton(
-                child: Text("Register"),
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/signup');
-                },
-              ),
-            ])));
+                children: mainWidgets(context))));
+  }
+
+  bool authenticate(){
+    setState(){
+      email = emailController.text;
+      password = passwordController.text;
+    }
+    return true;
+  }
+
+  mainWidgets(BuildContext context) {
+    return <Widget>[
+      FlutterLogo(size: 50.0),
+      SizedBox(height: 50.0),
+      TextFormField(
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0)),
+        ),
+      ),
+      SizedBox(height: 20.0),
+      TextFormField(
+        controller: passwordController,
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0)),
+        ),
+      ),
+      SizedBox(height: 35.0),
+      RaisedButton(
+        child: Text("Log in"),
+        onPressed: () {
+          if(authenticate()) {
+            Navigator.of(context).popAndPushNamed('/');
+          }
+        },
+      ),
+      SizedBox(height: 7.0),
+      RaisedButton(
+        child: Text("Register"),
+        onPressed: () {
+          if(authenticate()) {
+            Navigator.of(context).pushNamed('/signup');
+          }
+        },
+      ),
+    ];
   }
 }
--- ui/byteme/lib/signup.dart
+++ ui/byteme/lib/signup.dart
@@ -18,61 +18,62 @@ class _SignupState extends State<SignupPage> {
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              FlutterLogo(size: 50.0),
-              SizedBox(height: 50.0),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Repeat Password',
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-              ),
-              SizedBox(height: 7.0),
-              RaisedButton(
-                child: Text("Register"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                            content: Text("Successful!"),
-                            actions: <Widget>[
-                              RaisedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text("OK"))
-                            ]);
-                      });
-                },
-              ),
-            ])));
+                children: mainWidgets(context))));
+  }
+
+  mainWidgets(BuildContext context) {
+    return <Widget>[
+      FlutterLogo(size: 50.0),
+      SizedBox(height: 50.0),
+      TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+      ),
+      SizedBox(height: 20.0),
+      TextFormField(
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+      ),
+      SizedBox(height: 20.0),
+      TextFormField(
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Repeat Password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+      ),
+      SizedBox(height: 7.0),
+      RaisedButton(
+        child: Text("Register"),
+        onPressed: () {
+          Navigator.of(context).pop();
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    content: Text("Successful!"),
+                    actions: <Widget>[
+                      RaisedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("OK"))
+                    ]);
+              });
+        },
+      ),
+    ];
   }
 }
