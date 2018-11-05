c54dfa423a49250a13dec5498268b365863ad76f
Anders <anders.wk.choi@gmail.com>
2018-11-05 19:49:12 +0900

    ok

--- server/byteme/events/models.py
+++ server/byteme/events/models.py
@@ -4,14 +4,14 @@ from byteme import Speaker
 
 class Event(models.Model):
 	identifier = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	created = models.ForeignKey(UserProfile, on_delete=models.SET_NULL)
 	abstract = models.TextField(max_length=3000)
 	place = models.CharField(max_length=100)
 	time = models.DateTimeField()
 	title = models.CharField(max_length=300)
 	details = models.TextField()
 	tags = models.ManyToManyField(Tag)
-	speakers = models.ForeignKey(Speaker, on_delete=models.SET_NULL)
-	created = models.ForeignKey(UserProfile, on_delete=models.SET_NULL)
+	speaker = models.ForeignKey(Speaker, on_delete=models.SET_NULL)
 
 	REQUEST = (
 		('non', 'No Request'),
@@ -25,6 +25,8 @@ class Event(models.Model):
 	timeReq = models.DateTimeField(default=None, null=True)
 	titleReq = models.CharField(max_length=300, default=None, null=True)
 	detailsReq = models.TextField(default=None, null=True)
+	tagsReq = models.ManyToManyField(Tag)
+	speakerReq = models.ForeignKey(Speaker, on_delete=models.SET_NULL)
 
 	rankingVector = models.DecimalField(decimal_places=0, max_digits=5, null=True)
 
