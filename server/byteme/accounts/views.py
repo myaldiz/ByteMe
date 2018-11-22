4fed7d9f92f1e7fdc741d9fad1b0f182f0a2808a
Mustafa Berk YALDIZ <myaldiz@Mustafas-MacBook-Pro.local>
2018-11-23 07:02:18 +0900

    Bug fixes

--- server/byteme/accounts/views.py
+++ server/byteme/accounts/views.py
@@ -32,14 +32,14 @@ def example_view(request):
 
 @api_view(['POST'])
 def CreateProfile(request):
-	try:
+    try:
         json_username = request.data.get("register").get("username")
         json_email = request.data.get("register").get("email")
         json_password = request.data.get("register").get("password")
     except : 
         return Response({"Response":"Register", "status": "Please check response json"}, status=status.HTTP_400_BAD_REQUEST)
-	user = User.objects.create_user(json_username, email=json_email, password=json_password)
-	person = UserProfile.objects.create(user=user, isAdmin=False)
+    user = User.objects.create_user(json_username, email=json_email, password=json_password)
+    person = UserProfile.objects.create(user=user, isAdmin=False)
 
 def CreatePerson(profile):
 	pass
