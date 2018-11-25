e41472af4f5b63279e3db42774843e6b2eb6fcf8
Wu Harlem <harlemwu0930@gmail.com>
2018-11-25 16:49:47 +0900

    change tag function

--- server/byteme/events/tag_serializers.py
+++ server/byteme/events/tag_serializers.py
@@ -3,7 +3,6 @@ from .tag import Tag
 
 class TagSerializer(serializers.Serializer):
     name = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # rankingScore = serializers.DecimalField(required=False, decimal_places=0, max_digits=5)
 
     def create(self, validated_data):
         tag = Tag.objects.create(**validated_data)
@@ -11,6 +10,5 @@ class TagSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
-        instance.rankingScore = validated_data.get('rankingScore', instance.rankingScore)
         instance.save()
         return instance
