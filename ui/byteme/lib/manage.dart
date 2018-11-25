0667b7520753bc38a0ed3d6e54c343202530ddbf
Mohamed Elgaar <mohamed@kaist.ac.kr>
2018-11-25 14:32:46 +0900

    Remove filter button from manage page

--- ui/byteme/lib/manage.dart
+++ ui/byteme/lib/manage.dart
@@ -19,57 +19,41 @@ class ManagePageState extends State<ManagePage> {
   Widget build(BuildContext context) {
     if (_cardsList.length == 0) {
       updateList();
-      return Scaffold(
-        appBar: AppBar(),
-        body: Container());
+      return Scaffold(appBar: AppBar(), body: Container());
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Manage"),
-        actions: [
-        IconButton(
-            icon: Icon(Icons.sort),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return SortForm(onSubmit: (Tag newSort) {
-                      setState(() {
-                        selectedSort = newSort;
+        appBar: AppBar(
+          title: Text("Manage"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.sort),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return SortForm(onSubmit: (Tag newSort) {
+                        setState(() {
+                          selectedSort = newSort;
+                        });
                       });
                     });
-                  });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.filter_list),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return TagForm(onSubmit: (List<Tag> newList) {
-                      setState(() {
-                        selectedTags = newList;
-                      });
-                    });
-                  });
-            },
-          ),
-        IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => AddEventViewController()),
-              );
-            })
-      ],
-      ),
-      body: Container(
-        child: ListView(
-      children: _cardsList,
-    )));
+              },
+            ),
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddEventViewController()),
+                  );
+                })
+          ],
+        ),
+        body: Container(
+            child: ListView(
+          children: _cardsList,
+        )));
   }
 
   Future<void> updateList() async {
@@ -80,4 +64,3 @@ class ManagePageState extends State<ManagePage> {
     });
   }
 }
-
