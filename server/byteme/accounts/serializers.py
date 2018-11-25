66c64151e0548fbb44801573e348571ad6db6bfd
Wu Harlem <harlemwu0930@gmail.com>
2018-11-26 01:00:53 +0900

    add list_tag and get_profile json interface

--- server/byteme/accounts/serializers.py
+++ server/byteme/accounts/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username','email')
 
 class PersonSerializer(serializers.Serializer):
     univ = serializers.CharField(required=False, allow_blank=True, max_length=100)
