e642f6fbad5d90d9b956bd0ca1d1b43a1263a778
Anders <anders.wk.choi@gmail.com>
2018-11-25 19:18:21 +0900

    Update urls.py

--- server/byteme/accounts/urls.py
+++ server/byteme/accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^', include('rest_framework.urls')),
     url(r'^profile', views.GetProfile),
 	url(r'^register', views.CreateProfile),
+	url(r'^modify', views.ModifyProfile),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
