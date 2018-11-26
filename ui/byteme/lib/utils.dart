c6271ca0d1be1f0fb2e8b984ab3d37f641ebed77
Mohd Elgaar <mohamed@kaist.ac.kr>
2018-11-26 19:45:46 +0900

    Addevent

--- ui/byteme/lib/utils.dart
+++ ui/byteme/lib/utils.dart
@@ -166,3 +166,64 @@ class _SortFormState extends State<SortForm> {
     );
   }
 }
+
+class DateTimeItem extends StatelessWidget {
+  DateTimeItem({Key key, DateTime dateTime, @required this.onChanged})
+      : assert(onChanged != null),
+        date = dateTime == null
+            ? new DateTime.now()
+            : new DateTime(dateTime.year, dateTime.month, dateTime.day),
+        time = dateTime == null
+            ? new DateTime.now()
+            : new TimeOfDay(hour: dateTime.hour, minute: dateTime.minute),
+        super(key: key);
+
+  final DateTime date;
+  final TimeOfDay time;
+  final ValueChanged<DateTime> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Expanded(
+          child: new InkWell(
+            onTap: (() => _showDatePicker(context)),
+            child: new Padding(
+                padding: new EdgeInsets.symmetric(vertical: 8.0),
+                child: new Text("$date.day, $date.month $date.year"))),
+          ),
+        ),
+        new InkWell(
+          onTap: (() => _showTimePicker(context)),
+          child: new Padding(
+              padding: new EdgeInsets.symmetric(vertical: 8.0),
+              child: new Text('$time')),
+        ),
+      ],
+    );
+  }
+
+  Future _showDatePicker(BuildContext context) async {
+    DateTime dateTimePicked = await showDatePicker(
+        context: context,
+        initialDate: date,
+        firstDate: date.subtract(const Duration(days: 20000)),
+        lastDate: new DateTime.now());
+
+    if (dateTimePicked != null) {
+      onChanged(new DateTime(dateTimePicked.year, dateTimePicked.month,
+          dateTimePicked.day, time.hour, time.minute));
+    }
+  }
+
+  Future _showTimePicker(BuildContext context) async {
+    TimeOfDay timeOfDay =
+        await showTimePicker(context: context, initialTime: time);
+
+    if (timeOfDay != null) {
+      onChanged(new DateTime(
+          date.year, date.month, date.day, timeOfDay.hour, timeOfDay.minute));
+    }
+  }
+}
