e6a0e87d864393bf94fd61783ef885b19bb280fa
Wu Harlem <harlemwu0930@gmail.com>
2018-11-27 23:43:22 +0900

    add

--- ui/byteme/lib/createCardListBrowse.dart
+++ ui/byteme/lib/createCardListBrowse.dart
@@ -8,19 +8,11 @@ import './token.dart';
 List<Widget> createCardListBrowse(List events) {
   List<Widget> newCardsList = [];
   Widget card;
-<<<<<<< HEAD
   http.Response response = await http.get(
     Uri.encodeFull(url), headers: {"content-type": "application/json", "accept": "application/json", "Authorization": "Token " + token}
     );
   Map<String, dynamic> data = json.decode(response.body);
   if (data["Events"].isEmpty) {
-=======
-  // http.Response response = await http.get(
-  //   Uri.encodeFull(url), headers: {"content-type": "application/json", "accept": "application/json", "Authorization": "Token  " + "fc409decc5b05b43c39b8ec5b4de6a59d699afa2"}
-  //   );
-  // Map<String, dynamic> data = json.decode(response.body);
-  if (events.isEmpty) {
->>>>>>> b603e47f8675b6435d8e4e8bde9565e0351a6f82
     card = Card(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
