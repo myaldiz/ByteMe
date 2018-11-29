2e9fb1d2c9d3ae7f96247a7126b72ae21e0e78f8
Wu Harlem <harlemwu0930@gmail.com>
2018-11-29 12:30:35 +0900

    add webinterface

--- server/byteme/byteme/urls.py
+++ server/byteme/byteme/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/account/', include('accounts.urls')),
     url(r'^api/v1/event/', include('events.urls')),
+    url(r'^webinterface/', include('webinterface.urls')),
 ]
---
+++ server/byteme/webinterface/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect
+
+from events.models import Event
+from accounts.models import UserProfile
+from accounts.models import Speaker
+from events.tag import Tag
+from events.tag_serializers import TagSerializer
+from events.serializers import EventSerializer
+
+from django.contrib.auth.models import User
+from events.views import approveEventChange
+# Create your views here.
+def approve(request):
+    login_user = request.user #get login user
+    login_userprofile = UserProfile.objects.get(user = login_user) #get userprofile
+    event_created = Event.objects.filter(creater = login_userprofile)
+
+    if request.method == "POST":
+        event_id = request.POST["id"]
+        event = Event.objects.get(identifier = event_id)
+        approveEventChange(event_id, event.req)
+        return HttpResponseRedirect(next)
+    
+    return render(request, 'approve.html', locals())
