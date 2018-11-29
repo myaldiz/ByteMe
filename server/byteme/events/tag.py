bf4c3b21dcc76408500309b779ec9b463d3fe8b8
Wu Harlem <harlemwu0930@gmail.com>
2018-11-29 16:56:53 +0900

    modify tag

--- server/byteme/events/tag.py
+++ server/byteme/events/tag.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Tag(models.Model):
 	name = models.CharField(max_length=30)
-
+	class Meta:
+		ordering = ('name', )
 	def __str__(self):
 		return "%s"%(self.name)
 
